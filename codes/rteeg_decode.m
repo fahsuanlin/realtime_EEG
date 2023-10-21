4f1e2b5fc1640a8014e8e6f217e4faa1509d77cd
Fa-Hsuan Lin <fahsuanlin@gmail.come>
2023-10-20 22:10:34 -0500

    102023

--- codes/rteeg_decode.m
+++ codes/rteeg_decode.m
@@ -1,6 +1,6 @@
 function [buffer_decode,varargout]=rteeg_decode(buffer,varargin)
 buffer_decode=[];
-try
+%try
 
     buffer_decode.frameType=swapbytes(typecast(uint8(buffer(1)),'uint8'));
     buffer_decode.flag_ok=0;
@@ -31,13 +31,20 @@ try
             buffer_decode.numSampleBundles=swapbytes(typecast(uint8(buffer(11:12)),'uint16'));
             buffer_decode.firstSampleIndex=swapbytes(typecast(uint8(buffer(13:20)),'uint64'));
             buffer_decode.firstSampleTime=swapbytes(typecast(uint8(buffer(21:28)),'uint64'));
-            buffer_decode.sample=uint8(buffer(29:end));
+            %buffer_decode.sample=uint8(buffer(29:end));
+            buffer_decode.sample=uint8(buffer(29:28+3*buffer_decode.numChannels*buffer_decode.numSampleBundles));
 
             buffer_decode.sample=reshape(buffer_decode.sample,[3, length(buffer_decode.sample)/3]);
+            %fprintf('buffer=%s\n',mat2str(size(buffer)));
             buffer_decode.sample=rteeg_read_int24(buffer_decode.sample);
-            buffer_decode.sample=reshape(buffer_decode.sample, [buffer_decode.numChannels, length(buffer_decode.sample(:))/ buffer_decode.numChannels]);
-
             buffer_decode.flag_ok=1;
+            %try
+                buffer_decode.sample=reshape(buffer_decode.sample, [buffer_decode.numChannels, length(buffer_decode.sample(:))/ buffer_decode.numChannels]);
+            %catch
+            %    keyboard;
+            %    buffer_decode.sample=[];
+            %    buffer_decode.flag_ok=0;
+            %end;
 
         case 3 % Trigger Packets
             buffer_decode.mainUnitNum=swapbytes(typecast(uint8(buffer(2)),'uint8'));
@@ -48,7 +55,7 @@ try
                 buffer_decode.trigger(trigger_idx).sampleIndex=swapbytes(typecast(uint8(buffer(8+(trigger_idx-1)*20+9:8+(trigger_idx-1)*20+16)),'uint64'));
                 buffer_decode.trigger(trigger_idx).type=swapbytes(typecast(uint8(buffer(8+(trigger_idx-1)*20+17)),'uint8'));
                 buffer_decode.trigger(trigger_idx).code=swapbytes(typecast(uint8(buffer(8+(trigger_idx-1)*20+18)),'uint8'));
-                buffer_decode.trigger(trigger_idx).reserved=swapbytes(typecast(uint8(buffer(8+(trigger_idx-1)*20+19:trigger_idx-1)*20+20)),'uint16'));
+                buffer_decode.trigger(trigger_idx).reserved=swapbytes(typecast(uint8(buffer(8+(trigger_idx-1)*20+19:(trigger_idx-1)*20+20)),'uint16'));
             end;
 
             buffer_decode.flag_ok=1;
@@ -76,9 +83,9 @@ try
         otherwise
             %not defined....
     end;
-catch
-    %error in decoding
-end;
+%catch
+%    %error in decoding
+%end;
 
 
 return;
